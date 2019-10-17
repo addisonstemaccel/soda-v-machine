e228676019a84b3bd1f6ffcf50ff290df5fefce1
darrick addison <kunal@Kunals-MacBook-Pro.local>
2019-10-17 17:52:27 +0530

    Implemented vending machine simulation

---
+++ src/com/soda/machine/DataHandler.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soda.machine;
+
+public interface DataHandler {
+	 public boolean write(String productsFW, String totalFW, String inputFW);
+	 public void displayUsers(java.io.PrintStream out);
+	}
